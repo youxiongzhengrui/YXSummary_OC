2f86a3869e1ec7d9a1fc72f98a9bd36435912f6c
youxiongzhengrui <YXzr@YXzrdeMacBook-Pro.local>
2017-05-03 00:41:09 +0800

    first revise
    first revise: add the tool of cocoapod

---
+++ YXSummary_OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YXSummary_OC
+//
+//  Created by ~YXzr~ on 2017/4/27.
+//  Copyright © 2017年 ~YXzr~. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
