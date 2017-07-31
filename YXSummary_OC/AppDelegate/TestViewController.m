bdb3e07eb0d5da5d749775956f9f31c65399a9fe
xiongzhengrui <1058561676@qq.com>
2017-07-31 14:04:55 +0800

    test3

--- YXSummary_OC/AppDelegate/TestViewController.m
+++ YXSummary_OC/AppDelegate/TestViewController.m
@@ -19,6 +19,10 @@
     // Do any additional setup after loading the view.
 }
 
+- (void)test {
+    
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
